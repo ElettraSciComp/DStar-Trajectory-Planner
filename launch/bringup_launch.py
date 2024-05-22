628d96ff5c9f919897dca2a3170a423cf5ce3f40
Michele <>
2024-05-22 22:53:02 +0200

    set false composition to add time

--- launch/bringup_launch.py
+++ launch/bringup_launch.py
@@ -115,7 +115,8 @@ def generate_launch_description():
         description='Automatically startup the nav2 stack')
 
     declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition', default_value='True',
+        # 'use_composition', default_value='True',
+        'use_composition', default_value='False',
         description='Whether to use composed bringup')
 
     declare_use_respawn_cmd = DeclareLaunchArgument(
